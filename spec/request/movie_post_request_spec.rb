19d22cde82921f352ff2e7669f9bb450353ea506
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2020-07-05 20:14:04 -0300

    make-movie Add a validators spec
    -
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

--- spec/request/movie_post_request_spec.rb
+++ spec/request/movie_post_request_spec.rb
@@ -25,12 +25,28 @@ describe '::Api::V1::MovieController', type: :request do
     }
   end
 
-  let(:incomplete_params) do
+  let(:valid_params_with_11_actors) do
     params = {
       movie: {
         name: Faker::Lorem.word,
         release_date: 2.days.ago,
+        age_censure: 18,
         direction: Faker::Name.name,
+        actors: {
+          name: [
+           Faker::Name.name, Faker::Name.name, Faker::Name.name, Faker::Name.name,
+           Faker::Name.name, Faker::Name.name, Faker::Name.name, Faker::Name.name,
+           Faker::Name.name, Faker::Name.name, Faker::Name.name
+        ]
+        }
+      }
+    }
+  end
+
+
+  let(:incomplete_params) do
+    params = {
+      movie: {
         actors: {
           name: [Faker::Name.name, Faker::Name.name]
         }
@@ -54,12 +70,37 @@ describe '::Api::V1::MovieController', type: :request do
         expect(body['data']).to eq 'Bad Request'
       end
 
-      it 'msg in body mus be Age censure can`t be empty' do
-        expect(body['msg'].first).to eq 'Age censure can`t be empty'
+      it 'msg in body -> must be Name can`t be empty' do
+        expect(body['msg'].first).to eq 'Name can`t be empty'
       end
 
-      it 'msg in body Age censure must be an integer' do
-        expect(body['msg'].second).to eq 'Age censure must be an integer'
+      it 'msg in body -> must be Age censure can`t be empty' do
+        expect(body['msg'].second).to eq 'Age censure can`t be empty'
+      end
+
+      it 'msg in body -> Age censure must be an integer' do
+        expect(body['msg'].third).to eq 'Age censure must be an integer'
+      end
+
+      it 'msg in body -> Release date can`t be empty' do
+        expect(body['msg'].fourth).to eq 'Release date can`t be empty'
+      end
+
+      it 'msg in body -> Direction can`t be empty' do
+        expect(body['msg'].fifth).to eq 'Direction can`t be empty'
+      end
+
+      it 'msg in body -> This name already exists' do
+        post '/api/v1/movie', params: valid_params, headers: { 'ACCEPT' => 'application/json'}
+        post '/api/v1/movie', params: valid_params, headers: { 'ACCEPT' => 'application/json'}
+
+        expect(body['msg'].first).to eq 'This name already exists'
+      end
+
+      it 'msg in body -> A film can`t have more than 10 actors' do
+        post '/api/v1/movie', params: valid_params_with_11_actors, headers: { 'ACCEPT' => 'application/json'}
+
+        expect(body['msg']).to eq 'A film can`t have more than 10 actors'
       end
     end
 
